b4b5ac62848d66e4e84534a8243bf73837f78bdd
Elias Rosch <eliasrosch@googlemail.com>
2016-10-16 20:15:50 +0200

    Delete, plot update, hist stats..

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PyQt4 import QtCore
 import sys
 from cubetimer import Ui_TabWidget
 from state import CubeTimerState
+from PyKDE4.kdeui import KPlotWidget, KPlotObject
 
 class CubeTimer(QtGui.QTabWidget):
     def __init__(self):
@@ -34,7 +35,10 @@ class CubeTimer(QtGui.QTabWidget):
 
     def connectModels(self):
         self.cuTi_ui.lV_history.setModel(self.state.history.timeListModel)
+        self.updateHistPlot()
         self.cuTi_ui.lV_times.setModel(self.state.timeListModel)
+        self.cuTi_ui.kplotwidget.setAntialiasing(True)
+        self.cuTi_ui.kplotwidget.addPlotObject(self.state.history.plot_obj)
 
         self.cuTi_ui.tW_stats1.setItem(0,0, self.state.statMeanItem)
         self.cuTi_ui.tW_stats1.setItem(1,0, self.state.statAverageItem)
@@ -50,6 +54,14 @@ class CubeTimer(QtGui.QTabWidget):
         self.cuTi_ui.tW_stats2.setItem(4,0, self.state.statAo12Item)
         self.cuTi_ui.tW_stats2.setItem(5,0, self.state.statBestAo12Item)
 
+        self.cuTi_ui.tW_histStats1.setItem(0,0, self.state.history.statNumOfSolItem)
+        self.cuTi_ui.tW_histStats1.setItem(1,0, self.state.history.statMeanItem)
+        self.cuTi_ui.tW_histStats1.setItem(2,0, self.state.history.statAverageItem)
+        self.cuTi_ui.tW_histStats1.setItem(3,0, self.state.history.statStdDevItem)
+        self.cuTi_ui.tW_histStats1.setItem(4,0, self.state.history.statBestItem)
+        self.cuTi_ui.tW_histStats1.setItem(5,0, self.state.history.statWorstItem)
+
+
     def keyPressEvent(self, e):
         if e.isAutoRepeat():
             e.ignore()
@@ -57,6 +69,7 @@ class CubeTimer(QtGui.QTabWidget):
             if self.state.chronoIsRunning:
                 self.state.stopChrono()
                 self.updateTime(live=False)
+                self.updateHistPlot()
                 self.cuTi_ui.l_scramble.setText(self.state.scramble.getNewScramble())
             else:
                 self.cuTi_ui.l_chronoTime.setStyleSheet('color:#00CF36')
@@ -90,10 +103,17 @@ class CubeTimer(QtGui.QTabWidget):
                 self.cuTi_ui.pBar_mean.setValue(cur_perc)
                 self.cuTi_ui.pBar_overtime.setValue(0)
 
+    def updateHistPlot(self):
+        self.cuTi_ui.kplotwidget.setLimits( 0.0, len(self.state.history.timeList), 0.0, self.state.history.statWorst/1000);
+        self.cuTi_ui.kplotwidget.update()
+
     def slot_timeDeleteClick(self):
         sel_idx = self.cuTi_ui.lV_times.selectedIndexes()[0].row()
-        self.state.timeListModel.removeRows(sel_idx, 1)
-        self.state.timeList.pop((len(self.state.timeList)-1) - sel_idx)
+        self.state.deleteTime(sel_idx)
+        #self.state.timeListModel.removeRows(sel_idx, 1)
+        #self.state.timeList.pop((len(self.state.timeList)-1) - sel_idx)
+        #self.state.calcStats()
+
         # Debug for delete button
         #for i in range(len(self.state.timeList)):
         #     print("item-" + str(i) + " " + str(self.state.timeList[i]))
